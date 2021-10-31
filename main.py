bf5a48160e6b993ebf8d95a3494fc68e0240651c
Toska12138 <si.y.li@outlook.com>
2021-11-01 01:34:17 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import open3d as o3d
+import numpy as np
+from numpy import copy
+
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4459,
+                                      front=[0.9288, -0.2951, -0.2242],
+                                      lookat=[1.6784, 2.0612, 1.4451],
+                                      up=[-0.3402, -0.9189, -0.1996])
+
+
+source = o3d.io.read_point_cloud("./test_data/ICP/cloud_bin_0.pcd")
+target = o3d.io.read_point_cloud("./test_data/ICP/cloud_bin_1.pcd")
+threshold = 0.02
+trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
+                         [-0.139, 0.967, -0.215, 0.7],
+                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+draw_registration_result(source, target, trans_init)
